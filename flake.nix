b1948326751e99998a7518f1c6a2d7643309ae81
Chris Denneen <cdenneen@gmail.com>
2024-12-18 08:43:27 -0500

    Fix typo

--- flake.nix
+++ flake.nix
@@ -77,7 +77,7 @@
       );
 
       defaultPackage = {
-        x86_64-linux = home-manger.defaultPackage.x86_64-linux;
+        x86_64-linux = home-manager.defaultPackage.x86_64-linux;
         aarch64-linux = home-manager.defaultPackage.aarch64-linux;
         x86_64-darwin = home-manager.defaultPackage.x86_64-darwin;
         aarch64-darwin = home-manager.defaultPackage.aarch64-darwin;
